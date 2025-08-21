88237171813bf1f37df06b6138e8f37014445c75
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-08-22 05:19:12 +0800

    Add echoing functionality

--- src/main/java/TsundereChan.java
+++ src/main/java/TsundereChan.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class TsundereChan {
     public static void main(String[] args) {
         String horizontalLine = "_________________________________________";
         System.out.println(horizontalLine
                 + "\nHmph! I'm Tsundere-chan.\nWhat do you want? No-Not like I want to help you or anything...\n"
-                + horizontalLine
-                + "\nHmph, done already? D-don't talk to me anymore, you fool! It's not like I enjoyed that or anything...\n"
+                + horizontalLine);
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command + "\n" + horizontalLine);
+            command = sc.nextLine();
+        }
+        System.out.println(horizontalLine + "\nHmph, done already? D-don't talk to me anymore, you fool! It's not like I enjoyed that or anything...\n"
                 + horizontalLine);
     }
 }
