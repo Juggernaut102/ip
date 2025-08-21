92eec63872791b288f79c180d743f1ef829d10d2
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-08-22 04:45:53 +0800

    Rename file, add greet and exit text

---
+++ src/main/java/TsundereChan.java
@@ -0,0 +1,10 @@
+public class TsundereChan {
+    public static void main(String[] args) {
+        String horizontalLine = "_________________________________________";
+        System.out.println(horizontalLine
+                + "\nHmph! I'm Tsundere-chan.\nWhat do you want? No-Not like I want to help you or anything...\n"
+                + horizontalLine
+                + "\nHmph, done already? D-don't talk to me anymore, you fool! It's not like I enjoyed that or anything...\n"
+                + horizontalLine);
+    }
+}
