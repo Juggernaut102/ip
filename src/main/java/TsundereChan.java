0ad01565c6ab9482ca1418d22802e91b31ac19d3
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-08-23 04:49:03 +0800

    Refactor line variable to be a constant

--- src/main/java/TsundereChan.java
+++ src/main/java/TsundereChan.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
 public class TsundereChan {
+    private static final String LINE = "***************************************";
     public static void main(String[] args) {
-        String horizontalLine = "_________________________________________";
-        System.out.println(horizontalLine
+        System.out.println(LINE
                 + "\nHmph! I'm Tsundere-chan.\nWhat do you want? No-Not like I want to help you or anything...\n"
-                + horizontalLine);
+                + LINE);
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(command + "\n" + horizontalLine);
+            System.out.println(command + "\n" + LINE);
             command = sc.nextLine();
         }
-        System.out.println(horizontalLine + "\nHmph, done already? D-don't talk to me anymore, you fool! It's not like I enjoyed that or anything...\n"
-                + horizontalLine);
+        System.out.println(LINE + "\nHmph, done already? D-don't talk to me anymore, you fool!" +
+                "It's not like I enjoy talking to you or anything...\n"
+                + LINE);
     }
 }
