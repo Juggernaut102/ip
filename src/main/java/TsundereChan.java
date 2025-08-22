ddc10d5e9992743bf81e142303992c2b4a8babe2
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-08-23 05:01:44 +0800

    Refactor logic of program out of main method

--- src/main/java/TsundereChan.java
+++ src/main/java/TsundereChan.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class TsundereChan {
-    private static final String LINE = "***************************************";
-    public static void main(String[] args) {
+    private final String LINE = "***************************************";
+    public void run() {
         System.out.println(LINE
-                + "\nHmph! I'm Tsundere-chan.\nWhat do you want? No-Not like I want to help you or anything...\n"
+                + "\nHmph! I'm Tsundere-chan.\nWhat do you want? N-Not like I want to help you or anything...\n"
                 + LINE);
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
@@ -12,8 +12,12 @@ public class TsundereChan {
             System.out.println(LINE + "\n" + command + "\n" + LINE);
             command = sc.nextLine();
         }
-        System.out.println(LINE + "\nHmph, done already? D-don't talk to me anymore, you fool!" +
+        System.out.println(LINE + "\nHmph, done already? D-don't talk to me anymore, you fool!\n" +
                 "It's not like I enjoy talking to you or anything...\n"
                 + LINE);
+    }
+    public static void main(String[] args) {
+        TsundereChan tsundereChan = new TsundereChan();
+        tsundereChan.run();
     }
 }
