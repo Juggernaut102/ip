8596b972277bfae95c03a1d1b51727de5ffa57cb
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-08-23 05:48:26 +0800

    Add support for Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,18 @@
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : "";
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
