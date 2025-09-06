8ce5bfbab1b79dbfffc7ff57be8c91a2fd478127
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-09-07 00:01:16 +0800

    Add base support for GUI

--- src/main/java/tsunderechan/TsundereChan.java
+++ src/main/java/tsunderechan/TsundereChan.java
@@ -61,4 +61,11 @@ public class TsundereChan {
         TsundereChan tsundereChan = new TsundereChan("data/TsundereChan.txt");
         tsundereChan.run();
     }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
 }
